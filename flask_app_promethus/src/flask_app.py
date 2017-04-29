6e7519ef4addad4b09b22e7a91723e577b682bbd
Amit Saha <amitsaha.in@gmail.com>
2017-04-30 00:17:34 +1000

    Export prometheus metrics

--- flask_app_promethus/src/flask_app.py
+++ flask_app_promethus/src/flask_app.py
@@ -1,5 +1,9 @@
-from flask import Flask
-from middleware import setup_metrics
+from flask import Flask, Response
+from helpers.middleware import setup_metrics
+import prometheus_client
+
+CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
+
 
 app = Flask(__name__)
 setup_metrics(app)
@@ -16,6 +20,10 @@ def test1():
 @app.errorhandler(500)
 def handle_500(error):
     return str(error), 500
+
+@app.route('/metrics/')
+def metrics():
+    return Response(prometheus_client.generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run()
--- flask_app_promethus/src/helpers/middleware.py
+++ flask_app_promethus/src/helpers/middleware.py
@@ -1,18 +1,27 @@
 from flask import request
+from prometheus_client import Counter, Histogram
 import time
 import sys
+
+REQUEST_COUNT = Counter(
+    'request_count', 'App Request Count',
+    ['app_name', 'method', 'endpoint', 'http_status']
+)
+REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency',
+    ['app_name', 'endpoint']
+)
 
 def start_timer():
     request.start_time = time.time()
 
 def stop_timer(response):
     resp_time = time.time() - request.start_time
-    sys.stderr.write("Response time: %ss\n" % resp_time)
+    REQUEST_LATENCY.labels('webapp', request.path).observe(resp_time)
     return response
 
 def record_request_data(response):
-    sys.stderr.write("Request path: %s Request method: %s Response status: %s\n" %
-            (request.path, request.method, response.status_code))
+    REQUEST_COUNT.labels('webapp', request.method, request.path,
+            response.status_code).inc()
     return response
 
 def setup_metrics(app):
